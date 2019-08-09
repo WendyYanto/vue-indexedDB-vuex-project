d340bcd3a9b6999a206f5c5be072a71683d725a6
wendy.yanto <yanto_wendy67@yahoo.com>
2019-08-09 17:41:55 +0700

    Initial Commit

--- src/store.js
+++ src/store.js
@@ -5,12 +5,34 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    title: "My Current Title",
+    links: [
+      'http://google.com',
+      'http://coursetro.com',
+      'http://youtube.com'
+    ]
+  },
+  getters:{
+    countLinks: state => {
+      return state.links.length
+    }
   },
   mutations: {
-
+    ADD_LINK: (state, link) => {
+      state.links.push(link)
+    },
+    REMOVE_ALL: state => {
+      state.links = []
+    }
   },
   actions: {
-
+    removeAll({commit}) {
+      return new Promise( (resolve, reject) => {
+        setTimeout( () => {
+          commit('REMOVE_ALL')
+          resolve()
+        }, 1500)
+      })
+    }
   },
 });

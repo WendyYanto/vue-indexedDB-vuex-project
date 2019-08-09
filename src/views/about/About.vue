9024a088d39ef484ca063d72578a58d0ab052c76
WendyYanto <yanto.wendy98@gmail.com>
2019-08-09 17:41:55 +0700

    Initial Commit

---
+++ src/views/about/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <button @click="reset">Reset</button>
+    <h1>{{title}}</h1>
+    <ul>
+      <li v-for="link in links">{{link}}</li>
+    </ul>
+    <p>Total Current Link is {{countLinks}} </p>
+
+    <form @submit.prevent="addLink">
+      <input type="text" v-model="newLink" />
+    </form>
+  </div>
+</template>
+
+<script src="./about.js"></script>
